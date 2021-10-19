4c1b85adcf1562fb428cf9e34f80d8096d4b6491
Robby Noyes <robertjnoyes1@gmail.com>
2021-10-19 15:57:29 -0700

    images update

--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -3,7 +3,7 @@ const MainContent = () => {
     <div className="flex flex-col justify-center items-center lg:grid lg:grid-cols-thirds lg:items-stretch h-full">
       <div className="pt-16 lg:pt-0">
         <img
-          src="/images/imagedark.jpg"
+          src="./images/imagedark.jpg"
           alt=""
           className="lg:h-full object-cover lg:w-full"
         />
@@ -21,7 +21,7 @@ const MainContent = () => {
       </div>
       <div className="pt-16 lg:pt-0">
         <img
-          src="/images/imagelight.jpg"
+          src="./images/imagelight.jpg"
           alt=""
           className="lg:h-full object-cover lg:w-full"
         />
--- src/components/utilities/ImagePicker.js
+++ src/components/utilities/ImagePicker.js
@@ -4,17 +4,17 @@ import Tile from './Tile';
 const ImagePicker = () => {
   const [image] = useState([
     {
-      src: '/images/mobile1.jpg',
+      src: './images/mobile1.jpg',
       title: 'Discover innovative ways to decorate',
       text: 'We provide unmatched quality, comfort, and style for property owners across the country. Our experts combine form and function in bringing your vision to life. Create a room in your own style with our collection and make your property a reflection of you and what you love.',
     },
     {
-      src: '/images/mobile2.jpg',
+      src: './images/mobile2.jpg',
       title: 'We are available all across the globe',
       text: "With stores all over the world, it's easy for you to find furniture for your home or place of business. Locally, we’re in most major cities throughout the country. Find the branch nearest you using our store locator. Any questions? Don't hesitate to contact us today.",
     },
     {
-      src: '/images/mobile3.jpg',
+      src: './images/mobile3.jpg',
       title: 'Manufactured with the best materials',
       text: 'Our modern furniture store provide a high level of quality. Our company has invested in advanced technology to ensure that every product is made as perfect and as consistent as possible. With three decades of experience in this industry, we understand what customers want for their home and office.',
     },
@@ -22,17 +22,17 @@ const ImagePicker = () => {
 
   const [largeImage] = useState([
     {
-      src: '/images/desktop1.jpg',
+      src: './images/desktop1.jpg',
       title: 'Discover innovative ways to decorate',
       text: 'We provide unmatched quality, comfort, and style for property owners across the country. Our experts combine form and function in bringing your vision to life. Create a room in your own style with our collection and make your property a reflection of you and what you love.',
     },
     {
-      src: '/images/desktop2.jpg',
+      src: './images/desktop2.jpg',
       title: 'We are available all across the globe',
       text: "With stores all over the world, it's easy for you to find furniture for your home or place of business. Locally, we’re in most major cities throughout the country. Find the branch nearest you using our store locator. Any questions? Don't hesitate to contact us today.",
     },
     {
-      src: '/images/desktop3.jpg',
+      src: './images/desktop3.jpg',
       title: 'Manufactured with the best materials',
       text: 'Our modern furniture store provide a high level of quality. Our company has invested in advanced technology to ensure that every product is made as perfect and as consistent as possible. With three decades of experience in this industry, we understand what customers want for their home and office.',
     },
@@ -84,7 +84,7 @@ const ImagePicker = () => {
           <Tile header={currentImage.title} text={currentImage.text} />
           <button className="text-sm px-8 mt-4 py-4 tracking-realwide flex items-center justify-center lg:px-14">
             SHOP NOW
-            <img className="px-8" src="/images/icon-arrow.svg" alt="" />
+            <img className="px-8" src="./images/icon-arrow.svg" alt="" />
           </button>
         </div>
         <div className="hidden lg:flex lg:flex-row">
